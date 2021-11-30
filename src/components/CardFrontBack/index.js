5edfdfdb80a089a5b31a4c5d89431a9f5ca15390
Guilherme Vinicios Tramontin <gvtramontin@gmail.com>
2021-11-30 17:41:21 -0300

    Created the back face and the front face of the card: Put the CSS and created the function to turn the card

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,11 +1,31 @@
 import CardGame from "../CardGame";
+import "./style.css";
+
 
 
 function CardFrontBack() {
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        // if ($cardFrontBack.classList.contais('-active')) {
+        //     $cardFrontBack.classList.remove('-active')
+        // } else {
+        //     $cardFrontBack.classList.add('-active')
+        // }
+
+        $cardFrontBack.classList.toggle('-active');
+    }
+
     return `
-        <article class="card-front-back">
+        <article class="card-front-back" onClick="cardFrontBack.handleClick(event)">
+         <div class="card -front">
             ${CardGame()}
+         </div>
+         <div class="card -back">
             ${CardGame("javascript", "Logo do Javascript")}
+         </div>
         </article>
     `
 }
